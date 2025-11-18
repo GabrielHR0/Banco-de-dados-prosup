e6b7662f310bbdb3e4c4c99df9f5ebc9fd0e59f0
Jales <55924744+GabrielHR0@users.noreply.github.com>
2025-11-18 14:00:39 -0300

    resolvidos problemas no seed

--- seed_data/008_seed_plano.sql
+++ seed_data/008_seed_plano.sql
@@ -1,11 +1,6 @@
 INSERT INTO plano (nome, duracao_meses) VALUES
-('Plano Light (1x semana)', 1),
-('Plano Básico (2x semana)', 1),
-('Plano Standard (3x semana)', 1),
-('Plano Premium (Livre acesso)', 1),
-('Plano Casal Standard', 1),
-('Plano Família (4 pessoas)', 1),
-('Plano Trimestral Standard', 3),
-('Plano Semestral Premium', 6),
-('Plano Anual Diamond', 12),
-('Plano Corporativo (Empresas)', 12);
+('Plano Mensal', 1),
+('Plano Bimestral', 2),
+('Plano Trimestral', 3),
+('Plano Semestral', 6),
+('Plano Anual', 12);
--- seed_data/010_seed_matricula.sql
+++ seed_data/010_seed_matricula.sql
@@ -1,15 +1,25 @@
 INSERT INTO matricula (status, data_inicio, data_fim, aluno_id, plano_preco_id) VALUES
-('Ativa', '2024-01-10', '2024-07-10', 1, 2),
-('Ativa', '2024-02-15', '2024-08-15', 2, 4),
-('Ativa', '2024-03-01', '2024-09-01', 3, 6),
-('Ativa', '2024-01-20', '2025-01-20', 4, 9),
-('Ativa', '2024-02-01', '2024-08-01', 5, 3),
-('Ativa', '2024-03-10', '2024-09-10', 6, 5),
-('Ativa', '2024-01-15', '2024-07-15', 7, 7),
-('Ativa', '2024-02-20', '2024-11-20', 8, 8),
-('Ativa', '2024-03-05', '2025-03-05', 9, 10),
-('Ativa', '2024-01-25', '2024-07-25', 10, 1),
-('Cancelada', '2024-01-05', '2024-02-05', 11, 1),
-('Suspensa', '2024-02-10', '2024-08-10', 12, 2),
-('Ativa', '2024-03-15', '2024-09-15', 13, 4),
-('Ativa', '2024-01-30', '2024-07-30', 14, 3);
+-- Matrículas com Plano Mensal (id_plano=1)
+('Ativa', '2024-03-01', '2024-04-01', 1, 1),   -- Plano Mensal na Tabela 2024-1
+('Ativa', '2024-04-01', '2024-05-01', 2, 6),   -- Plano Mensal na Tabela 2024-2
+
+-- Matrículas com Plano Bimestral (id_plano=2)  
+('Ativa', '2024-03-01', '2024-05-01', 3, 2),   -- Plano Bimestral na Tabela 2024-1
+('Ativa', '2024-04-01', '2024-06-01', 4, 7),   -- Plano Bimestral na Tabela 2024-2
+
+-- Matrículas com Plano Trimestral (id_plano=3)
+('Ativa', '2024-03-01', '2024-06-01', 5, 3),   -- Plano Trimestral na Tabela 2024-1
+('Ativa', '2024-04-01', '2024-07-01', 6, 8),   -- Plano Trimestral na Tabela 2024-2
+
+-- Matrículas com Plano Semestral (id_plano=4)
+('Ativa', '2024-03-01', '2024-09-01', 7, 4),   -- Plano Semestral na Tabela 2024-1
+('Ativa', '2024-04-01', '2024-10-01', 8, 9),   -- Plano Semestral na Tabela 2024-2
+
+-- Matrículas com Plano Anual (id_plano=5)
+('Ativa', '2024-03-01', '2025-03-01', 9, 5),   -- Plano Anual na Tabela 2024-1
+('Ativa', '2024-04-01', '2025-04-01', 10, 10), -- Plano Anual na Tabela 2024-2
+
+-- Matrículas promocionais (Tabela Verão)
+('Ativa', '2024-02-01', '2024-03-01', 11, 11), -- Plano Mensal Promocional
+('Cancelada', '2024-02-01', '2024-04-01', 12, 12), -- Plano Bimestral Promocional (cancelada)
+('Suspensa', '2024-02-01', '2024-05-01', 13, 13); -- Plano Trimestral Promocional (suspensa)
